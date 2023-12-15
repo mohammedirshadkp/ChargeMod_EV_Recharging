ebbe4e5d97de0ab6b924d227c941cadd876c14ee
irshad500 <mohammedirshadkp500@gmail.com>
2023-12-15 08:52:06 +0530

    otp completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,28 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'Core/global_variables.dart';
-import 'splash_screen.dart';
-
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    deviceHeight = MediaQuery.of(context).size.height;
-    deviceWidth = MediaQuery.of(context).size.width;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: SplashScreen(),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+//
+// import 'Core/global_variables.dart';
+// import 'splash_screen.dart';
+//
+// void main() {
+//   runApp(const ProviderScope(child: MyApp()));
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     deviceHeight = MediaQuery.of(context).size.height;
+//     deviceWidth = MediaQuery.of(context).size.width;
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: SplashScreen(),
+//     );
+//   }
+// }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,74 +1,74 @@
-import 'dart:async';
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-
-import 'Core/Constants.dart';
-import 'Core/pallete.dart';
-import 'View/Onboard/screen/onboardbase.dart';
-// Other imports...
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    navigateToNextPage();
-  }
-
-  void navigateToNextPage() {
-    Timer(
-      const Duration(seconds: 3), // Change the duration as needed
-      () {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) =>
-                OnBoardBase(), // Replace this with your desired destination
-          ),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Pallete.primaryColor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          const SizedBox(height: 10),
-          InkWell(
-            onTap: () {
-              navigateToNextPage(); // Optionally, navigate immediately on tap
-            },
-            child: Center(
-              child: SvgPicture.asset(
-                Constants.chargeMode,
-                width: 220,
-              ),
-            ),
-          ),
-          Column(
-            children: [
-              const SizedBox(
-                width: 220,
-                child: LinearProgressIndicator(
-                  color: Pallete.secondaryColor,
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Text("Connecting To ChargeMod"),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_svg/svg.dart';
+//
+// import 'Core/Constants.dart';
+// import 'Core/pallete.dart';
+// import 'View/Onboard/screen/onboardbase.dart';
+// // Other imports...
+//
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
+//
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     navigateToNextPage();
+//   }
+//
+//   void navigateToNextPage() {
+//     Timer(
+//       const Duration(seconds: 3), // Change the duration as needed
+//       () {
+//         Navigator.pushReplacement(
+//           context,
+//           MaterialPageRoute(
+//             builder: (context) =>
+//                 OnBoardBase(), // Replace this with your desired destination
+//           ),
+//         );
+//       },
+//     );
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Pallete.primaryColor,
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//         children: [
+//           const SizedBox(height: 10),
+//           InkWell(
+//             onTap: () {
+//               navigateToNextPage(); // Optionally, navigate immediately on tap
+//             },
+//             child: Center(
+//               child: SvgPicture.asset(
+//                 Constants.chargeMode,
+//                 width: 220,
+//               ),
+//             ),
+//           ),
+//           Column(
+//             children: [
+//               const SizedBox(
+//                 width: 220,
+//                 child: LinearProgressIndicator(
+//                   color: Pallete.secondaryColor,
+//                 ),
+//               ),
+//               const SizedBox(height: 10),
+//               const Text("Connecting To ChargeMod"),
+//             ],
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
