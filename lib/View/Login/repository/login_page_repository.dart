ab183acfcf0b37eb643360c4d0df2654b2977b39
irshad500 <mohammedirshadkp500@gmail.com>
2024-03-03 19:42:03 +0530

    bug resolved

--- lib/View/Login/repository/login_page_repository.dart
+++ lib/View/Login/repository/login_page_repository.dart
@@ -91,7 +91,7 @@ class LoginRepository {
         return null;
       }
     } catch (e) {
-      print('Exept0ion: $e');
+      print('Exeptiion: $e');
       return {'error': e.toString()};
     }
   }
