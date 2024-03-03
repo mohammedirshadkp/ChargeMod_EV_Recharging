9ac3b3e4b2fc76f4ff6c1c63db1c45caa7d86843
irshad500 <mohammedirshadkp500@gmail.com>
2024-03-03 19:41:52 +0530

    bug resolved

--- lib/View/Login/repository/login_page_repository.dart
+++ lib/View/Login/repository/login_page_repository.dart
@@ -84,7 +84,7 @@ class LoginRepository {
             builder: (context) => Homepage(),
           ),
         );
-        print("Verifction successful");
+        print("Verfction successful");
         return data;
       } else {
         print('Error: ${response.reasonPhrase}');
